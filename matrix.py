9cb4ae1b1db3a1d52a91e29fd0262cee7af02d1f
anilkumar512 <anilkumar9120255@gmail.com>
2024-10-04 20:42:39 +0530

    added patterns.py

--- matrix.py
+++ matrix.py
@@ -1,3 +1,4 @@
+'''indices or positions of specific elements in the matrix'''
 rows=int(input('Enter number of rows:'))
 columns=int(input('Enter number of columns:'))
 for row in range(rows):
@@ -5,7 +6,7 @@ for row in range(rows):
         print((row,col),end=" ")
     print()
 
-
+'''Entering values and display of matrix'''
 rows=int(input('Enter number of rows:'))
 columns=int(input('Enter number of columns:'))
 matrix=[]
@@ -14,39 +15,7 @@ for row in range(rows):
     for col in range(columns):
        value=int(input(f'Enter value for matrix[{row,col}]:'))
        matrix[row].append(value)
-# print(matrix)
 for index in range(len(matrix)):
     print(matrix[index])
 
 
-rows=int(input('Enter number of rows:'))
-columns=int(input('Enter number of columns:'))
-for row in range(rows):
-    for col in range(columns):
-        if row==col:
-            print("*",end=" ")
-        else:
-            print(" ",end=" ")
-    print()
-
-
-rows=int(input('Enter number of rows:'))
-columns=int(input('Enter number of columns:'))
-for row in range(rows):
-    for col in range(columns):
-        if col==0 or row==(rows-1):
-            print("*",end=" ")
-        else:
-            print(" ",end=" ")
-    print()
-
-
-rows=int(input('Enter number of rows:'))
-columns=int(input('Enter number of columns:'))
-for row in range(rows):
-    for col in range(columns):
-        if row==0 or col==0 or row==(rows-1) or col==(columns-1) or row==(rows-1):
-            print("*",end=" ")
-        else:
-            print(" ",end=" ")
-    print()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,6 @@
-percentage=float(input('Enter percentage:'))
-if percentage>90:
-    print('Grade is A')
-elif 80<percentage<90:
-    print('Grade is B')
-elif 60<=percentage<=80:
-    print('Grade is C')
-elif percentage<60:
-    print('Grade is D')
+rows=int(input('Enter number of rows:'))
+columns=int(input('Enter number of columns:'))
+for row in range(rows):
+    for col in range(row+1,columns):
+        print(row*"*")
+    print()
